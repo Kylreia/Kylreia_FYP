76178e06ae80a0d3755922e63cf1bb46e0470a30
Kylreia <rumbauakeith@gmail.com>
2024-04-10 14:43:19 +0100

    added intro

---
+++ Scripts/intro.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	
+	$AnimationPlayer.play("fade_in")
+	await get_tree().create_timer(6).timeout
+	$AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+	
