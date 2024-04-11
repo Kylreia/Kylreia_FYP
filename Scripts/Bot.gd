4493ec2b59bf92fed573474eb7d324e66acf5d05
Kylreia <rumbauakeith@gmail.com>
2024-04-11 15:12:16 +0100

    training room update

--- Scripts/Bot.gd
+++ Scripts/Bot.gd
@@ -1,4 +1,14 @@
 extends Node3D
 
+var current_health = 100
+var max_health = 100
+
+var defend = true
+
 func _ready():
 	get_node("AnimationPlayer").play("Idle")
+	set_health($VBoxContainer/ProgressBar, current_health, max_health)
+
+func set_health(progress_bar, health, max_health):
+	progress_bar.value = health
+	progress_bar.max_value = max_health
